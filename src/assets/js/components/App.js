ac226f4536c8ac049f75dfb945d660ccaf428bd5
Godwin Nwachukwu <nwgwin@gmail.com>
2022-04-27 07:38:55 +0100

    organise layout

--- src/assets/js/components/App.js
+++ src/assets/js/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="calculator">
+      <div className="app">
         <Calculator />
       </div>
     );
--- src/assets/js/components/Calculator.js
+++ src/assets/js/components/Calculator.js
@@ -8,9 +8,27 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Title</h1>
-        <div>Calculator</div>
+      <div className="calculator">
+        <div className="answer">0</div>
+        <div>AC</div>
+        <div>+-</div>
+        <div>%</div>
+        <div>/</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>x</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>-</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>+</div>
+        <div className="zero">0</div>
+        <div>.</div>
+        <div>=</div>
       </div>
     );
   }
